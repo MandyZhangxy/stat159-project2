fbeae85271e57d82b1599a39c5f6ac1899ea3cd0
MandyZhangxy <mandy.zhang512@gmail.com>
2016-11-04 16:37:25 -0700

    include more library packages

--- code/scripts/session-info-script.R
+++ code/scripts/session-info-script.R
@@ -1,8 +1,18 @@
+# include all R packages in your project
+library(devtools)
+library(knitr)
+library(rmarkdown)
+library(xtable)
+library(ggplot2)
+library(glmnet)
+library(pls)
 
-library(testthat)
-sink("../../session-info.txt")
-sessionInfo()
+sink("../../session-info.txt", append = TRUE)
+cat("Session Information\n\n")
+print(sessionInfo())
+devtools::session_info()
 sink()
+
 
 
 
